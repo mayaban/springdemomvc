ac8bd5e5913a9e707d44d016d692ef8f7f13e5fb
Abhinash Bastola <abastola@urgent.ly>
2019-09-11 15:57:19 -0400

    model and view

--- src/main/java/com/abc/controllers/LoginController.java
+++ src/main/java/com/abc/controllers/LoginController.java
@@ -1,16 +1,23 @@
 package com.abc.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller; 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.abc.dto.LoginDto;
 
 @Controller
 public class LoginController {
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String getDriverPage() {
+	public ModelAndView getDriverPage() {
 		System.out.println("I am inside login controller");
-		return "login";
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("login");
+		LoginDto loginDto = new LoginDto();
+		mv.addObject("loginObject", loginDto);
+		return mv;
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,11 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+	<h2>Welcome</h2>
+	<table align="center">
+		<tr>
+			<td><a href="login">Log In</a></td>
+			<td><a href="register">Register</a></td>
+		</tr>
+	</table>
 </body>
 </html>
